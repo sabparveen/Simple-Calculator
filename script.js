2e68aacdb2133e4526febe7bc3d8a4697a90a3e8
sabparveen <sabaparveen.xwave@gmail.com>
2024-08-21 14:06:25 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function appendToDisplay(value) {
+    const display = document.getElementById('display');
+    if (display.innerText === '0') {
+        display.innerText = value;
+    } else {
+        display.innerText += value;
+    }
+}
+
+function clearDisplay() {
+    document.getElementById('display').innerText = '0';
+}
+
+function deleteLast() {
+    const display = document.getElementById('display');
+    display.innerText = display.innerText.slice(0, -1) || '0';
+}
+
+function calculate() {
+    const display = document.getElementById('display');
+    try {
+        display.innerText = eval(display.innerText);
+    } catch {
+        display.innerText = 'Error';
+    }
+}
